14e6c708e0c03e7137f56e072aaa931f80bcb965
dennis-yin <dennis.yin26@gmail.com>
2019-11-25 22:02:01 -0800

    complete up to Listing Interviewers II

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  const dayClass = classNames( 'day-list__item', { 'day-list__item--selected': true, 'day-list__item--full': props.spots === 0 });
+
+  const formatSpots = function() {
+    let text = "";
+    if (props.spots === 0) {
+      text += "no spots remaining";
+    } else if (props.spots === 1) {
+      text += "1 spot remaining";
+    } else {
+      text += `${props.spots} spots remaining`;
+    }
+    return text;
+  }
+
+  const spotsRemaining = formatSpots();
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{spotsRemaining}</h3>
+    </li>
+  );
+}
---
+++ src/components/InterviewerList.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const { interviewers, selectedInterviewer, setInterviewer } = props;
+
+  const interviewerList = interviewers.map(interviewer =>
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === selectedInterviewer}
+      setInterviewer={setInterviewer}
+    />
+  )
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerList}</ul>
+    </section>
+  )
+}
