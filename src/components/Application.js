7ee9c5ba17f8a8f3db91765666f4e628f9d71c15
dennis-yin <dennis.yin26@gmail.com>
2019-11-26 22:09:02 -0800

    use axios to fetch days data from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList.js";
 import Appointment from "components/Appointment";
@@ -58,30 +59,23 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   const apptComponents = appointments.map(appointment => <Appointment
     key={appointment.id} {...appointment}/>)
 
   console.log(day)
+
+  useEffect(() => {
+    axios.get("api/days")
+      .then(response => {
+        setDays(response.data)
+      })
+      .catch(error => {
+        console.log("Failed to retrieve data")
+      })
+  }, [])  // Pass empty array so that this request only runs once
 
   return (
     <main className="layout">
